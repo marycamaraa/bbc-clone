4f2d8449923201dd6adcc03a1a154ae8d0e097ff
marycamaraa <mary.g.camara@bbc.co.uk>
2026-09-13 22:55:50 +0100

    Revert "feat: structure home page"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,36 +3,7 @@ import "./Home";
 function Home() {
   return (
     <>
-      <main className="container">
-        <h1>Welcome to the BBC</h1>
-        <div className="hontainer">
-          <h2> News Headlines</h2>
-          <ul>
-            <li>
-              <img src="src/assets/icons/placeholder.png" />
-              <h1>LIVE</h1>
-              <p>LIVE</p>
-              <img
-                src="src/assets/icons/news.svg"
-                width={"30px"}
-                height={"30px"}
-              />
-            </li>
-            <li>
-              <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            </li>
-            <li>
-              <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            </li>
-            <li>
-              <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            </li>
-            <li>
-              <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            </li>
-          </ul>
-        </div>
-      </main>
+      <h1>Welcome to the BBC</h1>
     </>
   );
 }
