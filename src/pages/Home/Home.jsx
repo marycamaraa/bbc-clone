6cdf0e65c1b34f5aeddf07b9c6468142cf7395b8
marycamaraa <mary.g.camara@bbc.co.uk>
2026-09-14 00:02:46 +0100

    feat: restructure content table

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,39 +1,67 @@
-import "./Home";
+import "./Home.css";
 
 function Home() {
   return (
-    <>
+    <div className="home-container">
       <main className="container">
         <h1>Welcome to the BBC</h1>
-        <div className="hontainer">
-          <h2> News Headlines</h2>
-          <ul>
-            <li>
-              <img src="src/assets/icons/placeholder.png" />
-              <h1>LIVE</h1>
-              <p>LIVE</p>
-              <img
-                src="src/assets/icons/news.svg"
-                width={"30px"}
-                height={"30px"}
-              />
-            </li>
-            <li>
-              <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            </li>
-            <li>
-              <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            </li>
-            <li>
-              <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            </li>
-            <li>
-              <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            </li>
-          </ul>
-        </div>
+    
+        <h2> News Headlines</h2>
+        {/* home list items */}
+        <ul className="home-list" display={"flex"}>
+          <li home-list-item>
+            <img src="src/assets/icons/placeholder.png" />
+            <h1>LIVE</h1>
+            <p>LIVE</p>
+            <img
+              src="src/assets/icons/news.svg"
+              width={"30px"}
+              height={"30px"}
+            />
+          </li>
+          <li>
+            <img src="src/assets/icons/placeholder.png" width={"300px"} />
+            <h1>LIVE</h1>
+            <p>LIVE</p>
+            <img
+              src="src/assets/icons/news.svg"
+              width={"30px"}
+              height={"30px"}
+            />
+          </li>
+          <li>
+            <img src="src/assets/icons/placeholder.png" width={"300px"} />
+            <h1>LIVE</h1>
+            <p>LIVE</p>
+            <img
+              src="src/assets/icons/news.svg"
+              width={"30px"}
+              height={"30px"}
+            />
+          </li>
+          <li>
+            <img src="src/assets/icons/placeholder.png" width={"300px"} />
+            <h1>LIVE</h1>
+            <p>LIVE</p>
+            <img
+              src="src/assets/icons/news.svg"
+              width={"30px"}
+              height={"30px"}
+            />
+          </li>
+          <li>
+            <img src="src/assets/icons/placeholder.png" width={"300px"} />
+            <h1>LIVE</h1>
+            <p>LIVE</p>
+            <img
+              src="src/assets/icons/news.svg"
+              width={"30px"}
+              height={"30px"}
+            />
+          </li>
+        </ul>
       </main>
-    </>
+    </div>
   );
 }
 
