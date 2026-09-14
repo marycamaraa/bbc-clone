e846928782fa2bc26953307a102d17e8f97bad95
marycamaraa <mary.g.camara@bbc.co.uk>
2026-09-14 22:13:01 +0100

    feat: add grid layouts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,67 +2,41 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="home-container">
-      <main className="container">
+    <div>
+      <div className="home-container">
         <h1>Welcome to the BBC</h1>
-    
-        <h2> News Headlines</h2>
-        {/* home list items */}
-        <ul className="home-list" display={"flex"}>
-          <li home-list-item>
-            <img src="src/assets/icons/placeholder.png" />
-            <h1>LIVE</h1>
-            <p>LIVE</p>
-            <img
-              src="src/assets/icons/news.svg"
-              width={"30px"}
-              height={"30px"}
-            />
-          </li>
-          <li>
-            <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            <h1>LIVE</h1>
-            <p>LIVE</p>
-            <img
-              src="src/assets/icons/news.svg"
-              width={"30px"}
-              height={"30px"}
-            />
-          </li>
-          <li>
-            <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            <h1>LIVE</h1>
-            <p>LIVE</p>
-            <img
-              src="src/assets/icons/news.svg"
-              width={"30px"}
-              height={"30px"}
-            />
-          </li>
-          <li>
-            <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            <h1>LIVE</h1>
-            <p>LIVE</p>
-            <img
-              src="src/assets/icons/news.svg"
-              width={"30px"}
-              height={"30px"}
-            />
-          </li>
-          <li>
-            <img src="src/assets/icons/placeholder.png" width={"300px"} />
-            <h1>LIVE</h1>
-            <p>LIVE</p>
-            <img
-              src="src/assets/icons/news.svg"
-              width={"30px"}
-              height={"30px"}
-            />
-          </li>
-        </ul>
-      </main>
+
+        <h2>News Headline</h2>
+        <div className="grid">
+          <div className="div1"></div>
+          <div className="div2">2</div>
+          <div className="div3">3</div>
+          <div className="div4">4</div>
+          <div className="div5">5</div>
+        </div>
+        <div>
+          <h2>Sport Headline</h2>
+          <div className="grid">
+            <div className="div1"></div>
+            <div className="div2">2</div>
+            <div className="div3">3</div>
+            <div className="div4">4</div>
+            <div className="div5">5</div>
+          </div>
+          <div></div>
+
+          <h2>Entertainment and TV </h2>
+          <div className="grid">
+            <div className="div1"></div>
+            <div className="div2">2</div>
+            <div className="div3">3</div>
+            <div className="div4">4</div>
+            <div className="div5">5</div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default Home;
