c87747c3f63c5a36452205d84ea6936c63be437b
marycamaraa <mary.g.camara@bbc.co.uk>
2026-09-15 08:28:30 +0100

    fix: grid structure update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,22 +16,22 @@ function Home() {
         </div>
         <div>
           <h2>Sport Headline</h2>
-          <div className="grid">
-            <div className="div1"></div>
-            <div className="div2">2</div>
-            <div className="div3">3</div>
-            <div className="div4">4</div>
-            <div className="div5">5</div>
+          <div className="gridOne">
+            <div className="Newsheadline1">1</div>
+            <div className="Newsheadline2">2</div>
+            <div className="Newsheadline3">3</div>
+            <div className="Newsheadline4">4</div>
+            <div className="Newsheadline5">5</div>
           </div>
           <div></div>
 
           <h2>Entertainment and TV </h2>
-          <div className="grid">
-            <div className="div1"></div>
-            <div className="div2">2</div>
-            <div className="div3">3</div>
-            <div className="div4">4</div>
-            <div className="div5">5</div>
+          <div className="gridThree">
+            <div className="entertainment1">1</div>
+            <div className="entertainment2">2</div>
+            <div className="entertainment3">3</div>
+            <div className="entertainment4">4</div>
+            <div className="entertainment5">5</div>
           </div>
           <div></div>
         </div>
