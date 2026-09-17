b5952cbd396b220911ba49db6becec13f8527b31
marycamaraa <mary.g.camara@bbc.co.uk>
2026-09-17 15:52:21 +0100

    feat: add a footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <ul className="footer-list">
+        <li>Terms of Use About</li>
+        <li>BBC Privacy Policy</li>
+        <li>Cookies Accessibility</li>
+        <li> Help Parental Guidance </li>
+        <li>Contact</li>
+        <li>BBC Make an editorial complaint </li>
+        <li>Careers </li>
+        <li>BBC emails for you</li>
+      </ul>
+      <p>
+        Copyright © 2026 BBC. The BBC is not responsible for the content of
+        external sites. Read about our approach to external linking.
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Footer from "./components/Footer/Footer.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <main>
+      <App />
+      <Footer />
+    </main>
   </StrictMode>,
-)
+);
